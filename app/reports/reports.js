f1f3f5546a2b96bfd54f7111e6000ec6c4a008ce
Jun Chen <junc76@gmail.com>
2015-07-17 18:02:18 +0800

    Add a checkbox for reports

--- app/reports/reports.js
+++ app/reports/reports.js
@@ -10,9 +10,30 @@ angular.module('myApp.reports', ['ngRoute'])
 }])
 
 .controller('ReportsCtrl', function($scope) {
-    $scope.reports = [
-        {id:115807296, bid:27260101, sent:false, defect:47988, email: 'karanth@mahindraaerospace.com', comments: 'abcd', bnotes: '1234' },
-        {id:115792788, bid:27260101, sent:false, defect:47988, email: 'studio.rosnati@libero.it', comments: 'efgh', bnotes: '1234' },
-        {id:115745129, bid:27260101, sent:false, defect:47988, email: 'sono@mikami.co.jp', comments: 'ijkl', bnotes: '1234' },
-    ];
+  $scope.reports = [
+    {id:115807296, bid:27260101, sent:false, defect:47988, email: 'karanth@mahindraaerospace.com', comments: 'abcd', bnotes: '1234', select: false },
+    {id:115792788, bid:27260101, sent:false, defect:47988, email: 'studio.rosnati@libero.it', comments: 'efgh', bnotes: '1234', select: false },
+    {id:115745129, bid:27260101, sent:false, defect:47988, email: 'sono@mikami.co.jp', comments: 'ijkl', bnotes: '1234', select: false },
+  ];
+
+  $scope.allFlag = false;
+
+  $scope.toggleAll = function(){
+    $scope.reports.forEach(function(item){
+      item.select = $scope.allFlag;
+    });
+  };
+})
+
+.filter("checked", function(){
+  return function(items) {
+    var returnArr = [];
+    angular.forEach(items, function(item){
+      if (item.select == true){
+        returnArr.push(item);
+      }
+    });
+    return returnArr;
+  }
 });
+
