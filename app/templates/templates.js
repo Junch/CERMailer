f8232c732dc607046c7759e45cfa890f74eab873
Jun - Home <junc76@gmail.com>
2015-08-07 22:52:57 +0800

    Add the page to show the email templates

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -9,6 +9,35 @@ angular.module('myApp.templates', ['ngRoute'])
   });
 }])
 
-.controller('TemplatesCtrl', [function() {
-
-}]);
+.controller('TemplatesCtrl', function($scope){
+  $scope.data.templates = [
+      {  id: 1,
+         name: "General",
+         author: "Jun Chen",
+         note: "A template for all products",
+         email: "jun.chen@autodesk.com"
+      },
+      {  id: 2,
+         name: "AutoCAD",
+         author: "Tom Cat",
+         note: "A template for AutoCAD",
+         email: "tom.cat@autodesk.com"
+      },
+      {  id: 3,
+         name: "Bug 64",
+         author: "Jun Chen",
+         note: "A template specified for the TFS bug 64",
+         email: "jun.chen@autodesk.com"
+      }
+    ];
+  
+  $scope.delete = function(id){
+    $scope.data.templates.forEach(function(item){
+      if (item.id == id){
+        alert("Are you sure to delete the template: " + item.name);
+        return;
+      }
+    });
+  }
+  
+});
